4954f68cea5ed310fc5ed4041f0366f105b91b77
Blake Smith <besmith43@gmail.com>
2023-01-19 10:55:15 -0600

    swapped to http requests

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-using System.Net;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => {
-	HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://:5000/");
+	using HttpResponseMessage response = new HttpClient().GetAsync("http://localhost:5000/api").Result;
 
-	$"Hello World from C# on { System.Net.Dns.GetHostEntry(string.Empty).HostName }\napi version 1.0";
+	return $"Hello World from C# on { System.Net.Dns.GetHostEntry(string.Empty).HostName }\napi version 1.0";
 	});
 
 app.Run();
