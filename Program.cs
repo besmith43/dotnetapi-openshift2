0485ee1de44039a450ae28f87e9c07aae66d57b1
Blake Smith <besmith43@users.noreply.github.com>
2023-01-19 13:27:34 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var app = builder.Build();
 app.MapGet("/", () => {
 	using HttpResponseMessage response = new HttpClient().GetAsync("http://dotnetapi-openshift-git.demo:5000/").Result;
 
-	return $"Hello World from C# on { System.Net.Dns.GetHostEntry(string.Empty).HostName }\napi version 1.0\n{ response }";
+	return $"Hello World from C# on { System.Net.Dns.GetHostEntry(string.Empty).HostName }\napi version 1.0\n{ response.content }";
 	});
 
 app.Run();
